1bc4d46e54acb52790839ebbbdde04f530690216
Victor Marques <victyrmarques@gmail.com>
2021-02-28 17:08:34 -0300

    Adição de CSS básico e ajuste no tamanho do tabuleiro

--- src/components/Chess.vue
+++ src/components/Chess.vue
@@ -1,155 +1,213 @@
 <template>
- <table class="chess-board">
-            <tbody>
-                <tr>
-                    <th></th>
-                    <th draggable="true" >a</th>
-                    <th>b</th>
-                    <th>c</th>
-                    <th>d</th>
-                    <th>e</th>
-                    <th>f</th>
-                    <th>g</th>
-                    <th>h</th>
-                </tr>
-                <tr>
-                    <th>8</th>
-                    <td id="a8" class="light"  @click ="getPosition($event)" >♜</td>
-                    <td id="b8"  class="dark"  @click ="getPosition($event)">♞</td>
-                    <td id="c8" class="light"  @click ="getPosition($event)">♝</td>
-                    <td  id="d8"  class="dark" @click ="getPosition($event)">♛</td>
-                    <td id="e8" class="light"  @click ="getPosition($event)">♚</td>
-                    <td  id="f8" class="dark"  @click ="getPosition($event)">♝</td>
-                    <td  id="g8" class="light" @click ="getPosition($event)">♞</td>
-                    <td id="h8"  class="dark"  @click ="getPosition($event)">♜</td>
-                </tr>
-                <tr>
-                    <th>7</th>
-                    <td id="a7" class="dark" @click ="getPosition($event)">♟</td>
-                    <td id="b7" class="light" @click ="getPosition($event)">♟</td>
-                    <td id="c7" class="dark" @click ="getPosition($event)" >♟</td>
-                    <td id="d7" class="light" @click ="getPosition($event)"  >♟</td>
-                    <td id="e7"  class="dark" @click ="getPosition($event)">♟</td>
-                    <td id="f7" class="light" @click ="getPosition($event)">♟</td>
-                    <td id="g7" class="dark" @click ="getPosition($event)">♟</td>
-                    <td  id="h7" class="light" @click ="getPosition($event)">♟</td>
-                </tr>
-                <tr>
-                    <th>6</th>
-                     <td class="light" @click ="getPosition($event)" id="a6" ></td>
-                    <td class="dark"  @click ="getPosition($event)" id="b6" ></td>
-                    <td class="light" @click ="getPosition($event)" id="c6" ></td>
-                    <td class="dark"  @click ="getPosition($event)" id="d6" ></td>
-                    <td class="light" @click ="getPosition($event)" id="f6" ></td>
-                    <td class="dark"  @click ="getPosition($event)" id="e6" ></td>
-                    <td class="light" @click ="getPosition($event)" id="g6" ></td>
-                    <td class="dark"  @click ="getPosition($event)" id="h6" ></td>
-                </tr>
-                <tr>
-                    <th>5</th>
-                     <td class="dark"      @click ="getPosition($event)" id="a5" ></td>
-                    <td class="light"     @click ="getPosition($event)" id="b5"></td>
-                    <td class="dark"      @click ="getPosition($event)" id="c5"></td>
-                    <td class="light"     @click ="getPosition($event)" id="d5"></td>
-                    <td class="dark"      @click ="getPosition($event)" id="f5"></td>
-                    <td class="light"     @click ="getPosition($event)" id="e5"></td>
-                    <td class="dark"      @click ="getPosition($event)" id="f5"></td>
-                    <td class="light"     @click ="getPosition($event)" id="g5"></td>
-                </tr>
-                <tr>
-                    <th>4</th>
-                    <td class="light" @click ="getPosition($event)" id="a4" ></td>
-                    <td class="dark"  @click ="getPosition($event)" id="b4" ></td>
-                    <td class="light" @click ="getPosition($event)" id="c4" ></td>
-                    <td class="dark"  @click ="getPosition($event)" id="d4" ></td>
-                    <td class="light" @click ="getPosition($event)" id="f4" ></td>
-                    <td class="dark"  @click ="getPosition($event)" id="e4" ></td>
-                    <td class="light" @click ="getPosition($event)" id="g4" ></td>
-                    <td class="dark"  @click ="getPosition($event)" id="h4" ></td>
-                </tr>
-                <tr>
-                    <th>3</th>
-                    <td class="dark"      @click ="getPosition($event)" id="a3" ></td>
-                    <td class="light"     @click ="getPosition($event)" id="b3"></td>
-                    <td class="dark"      @click ="getPosition($event)" id="c3"></td>
-                    <td class="light"     @click ="getPosition($event)" id="d3"></td>
-                    <td class="dark"      @click ="getPosition($event)" id="f3"></td>
-                    <td class="light"     @click ="getPosition($event)" id="e3"></td>
-                    <td class="dark"      @click ="getPosition($event)" id="f3"></td>
-                    <td class="light"     @click ="getPosition($event)" id="g3"></td>
-                </tr>
-                <tr>
-                    <th>2</th>
-                    <td  class="light"  @click ="getPosition($event)" id="a2" >♙</td>
-                    <td  class="dark"   @click ="getPosition($event)" id="b2"  >♙</td>
-                    <td   class="light" @click ="getPosition($event)" id="c2" >♙</td>
-                    <td   class="dark"  @click ="getPosition($event)" id="d2" >♙</td>
-                    <td   class="light" @click ="getPosition($event)" id="e2" >♙</td>
-                    <td   class="dark"  @click ="getPosition($event)" id="f2" >♙</td>
-                    <td  class="light"  @click ="getPosition($event)" id="g2" >♙</td>
-                    <td   class="dark"  @click ="getPosition($event)" id="h2">♙</td>
-                </tr>
-                <tr>
-                    <th>1</th>
-                    <td   @click ="getPosition($event)" id="a1" class="dark">♖</td>
-                    <td   @click ="getPosition($event)" id="b1" class="light">♘</td>
-                    <td   @click ="getPosition($event)" id="c1" class="dark">♗</td>
-                    <td   @click ="getPosition($event)" id="d1" class="light">♕</td>
-                    <td   @click ="getPosition($event)" id="e1" class="dark">♔</td>
-                    <td   @click ="getPosition($event)" id="f1" class="light">♗</td>
-                    <td   @click ="getPosition($event)" id="g1" class="dark">♘</td>
-                    <td   @click ="getPosition($event)" id="h1" class="light">♖</td>
-                </tr>
-            </tbody>
-        </table>
+    <div class="containerFull">
+        <div class="horizontal-position">
+            <div>8</div>
+            <div>7</div>
+            <div>6</div>
+            <div>5</div>
+            <div>4</div>
+            <div>3</div>
+            <div>2</div>
+            <div>1</div>
+        </div>
+        <div class="vertical-position">
+            <div>A</div>
+            <div>B</div>
+            <div>C</div>
+            <div>D</div>
+            <div>E</div>
+            <div>F</div>
+            <div>G</div>
+            <div>H</div>
+        </div>
+        <div class="container">
+            <div class="row line-1">
+                <div id="A8" @click ="getPosition($event)">♜</div>
+                <div id="B8" @click ="getPosition($event)">♞</div>
+                <div id="C8" @click ="getPosition($event)">♝</div>
+                <div id="D8" @click ="getPosition($event)">♛</div>
+                <div id="E8" @click ="getPosition($event)">♚</div>
+                <div id="F8" @click ="getPosition($event)">♝</div>
+                <div id="G8" @click ="getPosition($event)">♞</div>
+                <div id="H8" @click ="getPosition($event)">♜</div>
+            </div>
+            <div class="row line-2">
+                <div id="A7" @click ="getPosition($event)">♟</div>
+                <div id="B7" @click ="getPosition($event)">♟</div>
+                <div id="C7" @click ="getPosition($event)">♟</div>
+                <div id="D7" @click ="getPosition($event)">♟</div>
+                <div id="E7" @click ="getPosition($event)">♟</div>
+                <div id="F7" @click ="getPosition($event)">♟</div>
+                <div id="G7" @click ="getPosition($event)">♟</div>
+                <div id="H7" @click ="getPosition($event)">♟</div>
+            </div>
+            <div class="row line-1">
+                <div id="A6" @click ="getPosition($event)"></div>
+                <div id="B6" @click ="getPosition($event)"></div>
+                <div id="C6" @click ="getPosition($event)"></div>
+                <div id="D6" @click ="getPosition($event)"></div>
+                <div id="E6" @click ="getPosition($event)"></div>
+                <div id="F6" @click ="getPosition($event)"></div>
+                <div id="G6" @click ="getPosition($event)"></div>
+                <div id="H6" @click ="getPosition($event)"></div>
+            </div>
+            <div class="row line-2">
+                <div id="A5" @click ="getPosition($event)"></div>
+                <div id="B5" @click ="getPosition($event)"></div>
+                <div id="C5" @click ="getPosition($event)"></div>
+                <div id="D5" @click ="getPosition($event)"></div>
+                <div id="E5" @click ="getPosition($event)"></div>
+                <div id="F5" @click ="getPosition($event)"></div>
+                <div id="G5" @click ="getPosition($event)"></div>
+                <div id="H5" @click ="getPosition($event)"></div>
+            </div>
+            <div class="row line-1">
+                <div id="A4" @click ="getPosition($event)"></div>
+                <div id="B4" @click ="getPosition($event)"></div>
+                <div id="C4" @click ="getPosition($event)"></div>
+                <div id="D4" @click ="getPosition($event)"></div>
+                <div id="E4" @click ="getPosition($event)"></div>
+                <div id="F4" @click ="getPosition($event)"></div>
+                <div id="G4" @click ="getPosition($event)"></div>
+                <div id="H4" @click ="getPosition($event)"></div>
+            </div>
+            <div class="row line-2">
+                <div id="A3" @click ="getPosition($event)"></div>
+                <div id="B3" @click ="getPosition($event)"></div>
+                <div id="C3" @click ="getPosition($event)"></div>
+                <div id="D3" @click ="getPosition($event)"></div>
+                <div id="E3" @click ="getPosition($event)"></div>
+                <div id="F3" @click ="getPosition($event)"></div>
+                <div id="G3" @click ="getPosition($event)"></div>
+                <div id="H3" @click ="getPosition($event)"></div>
+            </div>
+            <div class="row line-1">
+                <div id="A2" @click ="getPosition($event)">♙</div>
+                <div id="B2" @click ="getPosition($event)">♙</div>
+                <div id="C2" @click ="getPosition($event)">♙</div>
+                <div id="D2" @click ="getPosition($event)">♙</div>
+                <div id="E2" @click ="getPosition($event)">♙</div>
+                <div id="F2" @click ="getPosition($event)">♙</div>
+                <div id="G2" @click ="getPosition($event)">♙</div>
+                <div id="H2" @click ="getPosition($event)">♙</div>
+            </div>
+            <div class="row line-2">
+                <div id="A1" @click ="getPosition($event)">♖</div>
+                <div id="B1" @click ="getPosition($event)">♘</div>
+                <div id="C1" @click ="getPosition($event)">♗</div>
+                <div id="D1" @click ="getPosition($event)">♕</div>
+                <div id="E1" @click ="getPosition($event)">♔</div>
+                <div id="F1" @click ="getPosition($event)">♗</div>
+                <div id="G1" @click ="getPosition($event)">♘</div>
+                <div id="H1" @click ="getPosition($event)">♖</div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-
-
-export default {
-  name: 'Chess',
-  data () {
-        return {
-          movePhase: false
+    export default {
+        name: 'Chess',
+        data () {
+                return {
+                movePhase: false
+                }
+            },
+        methods: {
+            getPosition:function(ev){
+            
+                if(!this.movePhase) {
+                    if(ev.target.innerText !=""){    
+                        let pos = ev.target.id ;
+                        let jogada={posicao:"",peca:""};
+                        jogada.peca = ev.target.innerText;
+                        ev.target.innerText= ""
+                        this.movePhase = true;
+                        jogada.posicao = pos ;
+                        localStorage.setItem("jogada",JSON.stringify(jogada));
+                    }else{
+                        console.log(this.movePhase);
+                        return;
+                    }
+                }else{
+                    let jogada =JSON.parse( localStorage.getItem("jogada"));
+                    ev.target.innerText = jogada.peca;
+                    this.movePhase = false;
+                }
+            }
         }
-    },
-  methods: {
-    getPosition:function(ev){
-       
-       if(!this.movePhase) {
-       if(ev.target.innerText !=""){    
-       let pos = ev.target.id ;
-       let jogada={posicao:"",peca:""};
-         jogada.peca = ev.target.innerText;
-         ev.target.innerText= ""
-         this.movePhase = true;
-        jogada.posicao = pos ;
-       localStorage.setItem("jogada",JSON.stringify(jogada));
-       }else{
-           console.log(this.movePhase) ;
-       }
-       }else{
-           let jogada =JSON.parse( localStorage.getItem("jogada"));
-           ev.target.innerText = jogada.peca;
-           this.movePhase = false;
-       }
     }
-  }
- 
-  
-}
 </script>
 
 <style>
-   .chess-board { border-spacing: 0; border-collapse: collapse; }
-            .chess-board th { padding: .5em; }
-            .chess-board th + th { border-bottom: 1px solid #000; }
-            .chess-board th:first-child,
-            .chess-board td:last-child { border-right: 1px solid #000; }
-            .chess-board tr:last-child td { border-bottom: 1px solid; }
-            .chess-board th:empty { border: none; }
-            .chess-board td { width: 1.5em; height: 1.5em; text-align: center; font-size: 32px; line-height: 0;}
-            .chess-board .light { background: #eee; }
-            .chess-board .dark { background: #aaa; }
+    *, *::before, *::after{
+        box-sizing: border-box; 
+        -webkit-box-sizing: border-box; 
+        -moz-box-sizing: border-box; 
+        -o-box-sizing: border-box;  
+    }
+
+    .containerFull {
+        margin: 0 auto;
+        width: 600px;
+        margin: 0 auto;
+        margin-top: 5px
+    }
+
+    /******** Indicação das casas no tabuleiro ********/
+    .vertical-position {
+        float: left;
+        text-align: center;
+        font-size: 2em;
+        justify-content: space-between;
+        display: flex;
+    }
+    .vertical-position div
+    {
+        float: left;
+        width: 82px;
+    }
+    .horizontal-position
+    {
+        position:absolute;
+        margin-left: -30px;
+        margin-top:62px;
+        font-size: 2em;
+        text-align: center;
+    }
+    .horizontal-position div
+    {
+        height: 82px;
+    }
+
+    /*******************  Tabuleiro ********************/
+    .container {
+        border: 3px solid #000;
+        width: 662px;
+        height: 662px;
+        border-radius: 5px;
+        cursor: pointer;
+        float: left;
+    }
+    .row {
+        clear: both;
+    }
+    .row div {
+        float: left;
+        width: 82px;
+        height: 82px;
+        border-left: 1.5px solid #000;
+        border-bottom: 1.5px solid #000;
+        text-align: center;
+        font-size: 350%;
+    }
+    .row div:first-child {
+        border-left: 0;
+    }
+    .line-1 div:nth-child(2n+1)  {
+        background: #aaa;
+    }
+    .line-2 div:nth-child(2n)  {
+        background: #aaa;
+    }
 </style>
