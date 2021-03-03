b87bb218cf3c453c419ffe36e3fc5360aa7693f8
VictorMatheus819 <victormatheus819@gmail.com>
2021-03-02 23:16:41 -0300

    revisão de movimentação

--- src/components/Chess.vue
+++ src/components/Chess.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="containerFull">
+    <div v-if="player=='branco'" class="containerFull">
         <div class="horizontal-position">
             <div>8</div>
             <div>7</div>
@@ -103,6 +103,114 @@
             </div>
         </div>
     </div>
+    <div v-else class="containerFull">
+        <div class="horizontal-position">
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+            <div>4</div>
+            <div>5</div>
+            <div>6</div>
+            <div>7</div>
+            <div>8</div>
+        </div>
+        <div class="vertical-position">
+            <div>H</div>
+            <div>G</div>
+            <div>F</div>
+            <div>D</div>
+            <div>E</div>
+            <div>C</div>
+            <div>B</div>
+            <div>A</div>
+        </div>
+        <div class="container">
+            
+            <div class="row line-2">
+                <div id="A1" @click ="getPosition($event)">♖</div>
+                <div id="B1" @click ="getPosition($event)">♘</div>
+                <div id="C1" @click ="getPosition($event)">♗</div>
+                <div id="D1" @click ="getPosition($event)">♕</div>
+                <div id="E1" @click ="getPosition($event)">♔</div>
+                <div id="F1" @click ="getPosition($event)">♗</div>
+                <div id="G1" @click ="getPosition($event)">♘</div>
+                <div id="H1" @click ="getPosition($event)">♖</div>
+            </div>
+            <div class="row line-1">
+                <div id="A2" @click ="getPosition($event)">♙</div>
+                <div id="B2" @click ="getPosition($event)">♙</div>
+                <div id="C2" @click ="getPosition($event)">♙</div>
+                <div id="D2" @click ="getPosition($event)">♙</div>
+                <div id="E2" @click ="getPosition($event)">♙</div>
+                <div id="F2" @click ="getPosition($event)">♙</div>
+                <div id="G2" @click ="getPosition($event)">♙</div>
+                <div id="H2" @click ="getPosition($event)">♙</div>
+            </div>
+            <div class="row line-2">
+                <div id="A3" @click ="getPosition($event)"></div>
+                <div id="B3" @click ="getPosition($event)"></div>
+                <div id="C3" @click ="getPosition($event)"></div>
+                <div id="D3" @click ="getPosition($event)"></div>
+                <div id="E3" @click ="getPosition($event)"></div>
+                <div id="F3" @click ="getPosition($event)"></div>
+                <div id="G3" @click ="getPosition($event)"></div>
+                <div id="H3" @click ="getPosition($event)"></div>
+            </div>
+            <div class="row line-1">
+                <div id="A4" @click ="getPosition($event)"></div>
+                <div id="B4" @click ="getPosition($event)"></div>
+                <div id="C4" @click ="getPosition($event)"></div>
+                <div id="D4" @click ="getPosition($event)"></div>
+                <div id="E4" @click ="getPosition($event)"></div>
+                <div id="F4" @click ="getPosition($event)"></div>
+                <div id="G4" @click ="getPosition($event)"></div>
+                <div id="H4" @click ="getPosition($event)"></div>
+            </div>
+            <div class="row line-2">
+                <div id="A5" @click ="getPosition($event)"></div>
+                <div id="B5" @click ="getPosition($event)"></div>
+                <div id="C5" @click ="getPosition($event)"></div>
+                <div id="D5" @click ="getPosition($event)"></div>
+                <div id="E5" @click ="getPosition($event)"></div>
+                <div id="F5" @click ="getPosition($event)"></div>
+                <div id="G5" @click ="getPosition($event)"></div>
+                <div id="H5" @click ="getPosition($event)"></div>
+            </div>
+            <div class="row line-1">
+                <div id="A6" @click ="getPosition($event)"></div>
+                <div id="B6" @click ="getPosition($event)"></div>
+                <div id="C6" @click ="getPosition($event)"></div>
+                <div id="D6" @click ="getPosition($event)"></div>
+                <div id="E6" @click ="getPosition($event)"></div>
+                <div id="F6" @click ="getPosition($event)"></div>
+                <div id="G6" @click ="getPosition($event)"></div>
+                <div id="H6" @click ="getPosition($event)"></div>
+            </div>
+             
+            <div class="row line-2">
+                <div id="A7" @click ="getPosition($event)">♟</div>
+                <div id="B7" @click ="getPosition($event)">♟</div>
+                <div id="C7" @click ="getPosition($event)">♟</div>
+                <div id="D7" @click ="getPosition($event)">♟</div>
+                <div id="E7" @click ="getPosition($event)">♟</div>
+                <div id="F7" @click ="getPosition($event)">♟</div>
+                <div id="G7" @click ="getPosition($event)">♟</div>
+                <div id="H7" @click ="getPosition($event)">♟</div>
+            </div>
+            <div class="row line-1">
+                <div id="A8" @click ="getPosition($event)">♜</div>
+                <div id="B8" @click ="getPosition($event)">♞</div>
+                <div id="C8" @click ="getPosition($event)">♝</div>
+                <div id="D8" @click ="getPosition($event)">♛</div>
+                <div id="E8" @click ="getPosition($event)">♚</div>
+                <div id="F8" @click ="getPosition($event)">♝</div>
+                <div id="G8" @click ="getPosition($event)">♞</div>
+                <div id="H7" @click ="getPosition($event)">♜</div>
+            </div>
+            
+        </div>
+    </div>
+    
 </template>
 
 <script>
@@ -110,7 +218,9 @@
         name: 'Chess',
         data () {
                 return {
-                movePhase: false
+                movePhase: false,
+                player : "preto",
+                previouspos:""
                 }
             },
         methods: {
@@ -119,21 +229,30 @@
                 if(!this.movePhase) {
                     if(ev.target.innerText !=""){    
                         let pos = ev.target.id ;
+                        this.previouspos=ev.target.id 
                         let jogada={posicao:"",peca:""};
                         jogada.peca = ev.target.innerText;
-                        ev.target.innerText= ""
                         this.movePhase = true;
                         jogada.posicao = pos ;
                         localStorage.setItem("jogada",JSON.stringify(jogada));
+                        this.paintNextPos()
                     }else{
-                        console.log(this.movePhase);
                         return;
                     }
                 }else{
                     let jogada =JSON.parse( localStorage.getItem("jogada"));
+                    document.getElementById(this.previouspos).innerHTML= " ";
                     ev.target.innerText = jogada.peca;
+                     this.removePaint()
                     this.movePhase = false;
                 }
+
+            },
+            paintNextPos:function(){
+                document.getElementById("H3").classList.add("greenie");
+            },
+            removePaint:function(){
+                document.getElementById("H3").classList.remove("greenie");
             }
         }
     }
@@ -179,6 +298,9 @@
     {
         height: 82px;
     }
+    .greenie{
+        background:#66ff99;
+    }
 
     /*******************  Tabuleiro ********************/
     .container {
@@ -210,4 +332,5 @@
     .line-2 div:nth-child(2n)  {
         background: #aaa;
     }
+     
 </style>
